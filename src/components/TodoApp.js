64b554fa4e2a80f8091799a9107c62aba9694bcb
karanara <karanara@mail.uc.edu>
2024-01-15 23:11:05 -0500

    React-Full Stack

---
+++ src/components/TodoApp.js
@@ -0,0 +1,13 @@
+import Header from "./Header";
+import Body from "./Body";
+import Footer from "./Footer";
+const TodoApp = ()=>{
+    return (
+        <div>
+            <Header/>
+            <Body/>
+            <Footer/>
+        </div>
+    )
+};
+export default TodoApp;
